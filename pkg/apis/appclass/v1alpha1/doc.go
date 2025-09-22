102bfd764b9c9106fdab10d9b13a23b2638ac20b
Jose Santos <josepedro.pereiradossantos@ugent.be>
2025-09-22 17:19:32 +0200

    api modifications

--- pkg/apis/appclass/v1alpha1/doc.go
+++ pkg/apis/appclass/v1alpha1/doc.go
@@ -1,6 +1,6 @@
 // +k8s:deepcopy-gen=package
 // +k8s:defaulter-gen=TypeMeta
-// +groupName=scheduling.diktyo.x-k8s.io
+// +groupName=appclass.diktyo.x-k8s.io
 
 // Package v1alpha1 is the v1alpha1 version of the API.
 package v1alpha1 // import "github.com/diktyo-io/appclass-api/pkg/apis/appclass/v1alpha1"
